5643f48986ee06b261ea9bbd5cd9b1d57d2d9bb1
Naxalov <uzbzarif@gmail.com>
2020-02-19 16:11:31 +0900

    error solvePnP

--- pose.py
+++ pose.py
@@ -7,7 +7,11 @@ mtx = np.array([[551.35925517, 0., 248.33988363],
                 [0., 551.84937767, 309.15983624],
                 [0., 0., 1.]], dtype=np.float32)
 
-
+# dist = np.array([[-6.26163541e-02, 1.10526699e+00 - 1.62313064e-02, -3.25249256e-03, -6.24637994e+00]],
+#                 dtype=np.float32)
+# mtx = np.array([[865.42285781, 0., 380.62296218],
+#                 [0., 867.40707348, 346.83874496],
+#                 [0., 0., 1.]], dtype=np.float32)
 # Photo
 
 def get_image_point(img):
@@ -21,14 +25,12 @@ def get_image_point(img):
     return points
 
 
-model_points = np.array([
-    [0.0, 0.0, 0.0],  # Nose tip
-    [1.0, 0.0, 0.0],  # Nose tip
-    [-1.0, 0.0, 0.0],  # Nose tip
-    [-1.0, -1.0, 0.0],  # Nose tip
-
-], np.float32)
-
+def draw(img, corners, imgpts):
+    corner = tuple(corners[2].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+    return img
 
 
 # Photo
@@ -42,7 +44,8 @@ img_path = os.path.join(PATH, images[0])
 
 # read image
 
-img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+img_rgb = cv2.imread(img_path, cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 
 # get image size
 
@@ -51,4 +54,35 @@ W, H = img.shape
 # resize image
 img = cv2.resize(img, (W // 4, H // 4))
 
+img_rgb = cv2.resize(img_rgb, (W // 4, H // 4))
 image_points = get_image_point(img)
+
+for i, (x, y) in enumerate(image_points.astype(int)):
+    # draw the circle in the output image, then draw a rectangle
+    # corresponding to the center of the circle
+    cv2.putText(img_rgb, "centroid: " + str(i), (x - 25, y - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+
+model_points = np.array([
+    [-4, 4, 0],
+    [4, 4, 0],
+    [-4, -4, 0],
+    [4, -4, 0],
+
+], np.float32)
+
+axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+
+(success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, mtx, dist)
+
+# project 3D points to image plane
+imgpts, jac = cv2.projectPoints(axis, rotation_vector, translation_vector, mtx, dist)
+
+img = draw(img_rgb, image_points, imgpts)
+cv2.imshow('img', img_rgb)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(success)
+
+print(image_points)
